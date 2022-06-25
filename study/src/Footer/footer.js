46f5dfc60d568cc6e83429d955efd33264ab9bf4
hanav52 <wnsrl8329@gmail.com>
2022-06-25 20:19:49 +0900

    footer부분과 npm 설치

---
+++ study/src/Footer/footer.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Footer() {
+  return (
+    <footer className="sticky-footer bg-white">
+      <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+          <span>AhaStudy Website 2022</span>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

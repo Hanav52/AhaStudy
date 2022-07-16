05210ac846d75100d60eb8344c8867ead1f1321d
hanav52 <wnsrl8329@gmail.com>
2022-07-16 13:48:27 +0900

    메뉴 위치 변경 및 메뉴 클릭시 카테고리 별 api 호출

--- my-app/src/NavBody/Nav1.js
+++ my-app/src/NavBody/Nav1.js
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Route, Link } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import { useEffect, useState } from "react";
-import Nav2 from "../Nav2/Nav2";
+import Nav2 from "./Nav2";
 import AccountMenu from './로그인후';
 import moment from 'moment';
 
@@ -14,13 +14,14 @@ function Nav1() {
     useEffect(() => {
         setVisible(window.localStorage.getItem("State"));
     },[]);
-    console.log(visible)
     return(
+        <div className='inner'>
         <div className="head-container">
             <div className="head-brand">
                 <Link to="/">Aha Study</Link>
             </div>
             {!visible ? <button type="button" className="head-blog btn btn lg btn-warning"><Link to="/login">로그인</Link></button> : <AccountMenu/>}
+        </div>
         </div>
     )
 }

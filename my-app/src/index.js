4c2218c6739e7f4508a8c03cf9bb10759bc0850b
hanav52 <wnsrl8329@gmail.com>
2022-07-18 02:51:09 +0900

    파일 이동 및 파일 삭제

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import InfinityScroll from './Body/무한스크롤/InfinityScroll';
 import reportWebVitals from './reportWebVitals';
-import SignUp from './Login&Register/Register';
 import Hole from './전체/통합 copy';
-import SignIn from './Login&Register/Login copy';
-import Join from './Login&Register/Join';
-import Test from './Login&Register/test';
-import Users from './Login&Register/ttest';
-import RealMain from './Body/BodyImage';
-import CustomizedDialogs from './글작성/Modaal';
-import CreateList from './글작성/BLob';
-import Survey from './글작성/ttest';
-import BodyForMenu from './Body/Body1~6/BodyForMenu1';
-import App from './Body/App';
-import App1 from './Body/Posts/Posts1';
 ReactDOM.render(
   <React.StrictMode>
     <Hole/>
--- my-app/src/전체/통합 copy.js
+++ my-app/src/전체/통합 copy.js
@@ -1,18 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BodyForMenu from '../Body/Body1~6/BodyForMenu1';
 import { BrowserRouter, BrowserRouter as Route, Link, Router, Switch } from "react-router-dom";
 import SignIn from '../Login&Register/Login';
 import React from 'react';
-import SignUp from '../Login&Register/Register';
 import Nav from '../NavBody/Nav';
 import RealMain from '../Body/BodyImage';
 import Join from '../Login&Register/Join';
-import Posts1 from '../Body/Posts/Posts1';
-import Posts2 from '../Body/Posts/Posts2';
-import Posts3 from '../Body/Posts/Posts3';
-import Posts4 from '../Body/Posts/Posts4';
-import Posts5 from '../Body/Posts/Posts5';
-import Posts6 from '../Body/Posts/Posts6';
+import Posts from '../Body/Posts/Posts';
 
 
 function Hole() {
@@ -31,23 +24,8 @@ function Hole() {
                 <Route path="/register">
                     <Join/>
                 </Route>
-                <Route path="/Posts1">
-                    <Posts1/>
-                </Route>
-                <Route path="/Posts2">
-                    <Posts2/>
-                </Route>
-                <Route path="/Posts3">
-                    <Posts3/>
-                </Route>
-                <Route path="/Posts4">
-                    <Posts4/>
-                </Route>
-                <Route path="/Posts5">
-                    <Posts5/>
-                </Route>
-                <Route path="/Posts6">
-                    <Posts6/>
+                <Route path="/post">
+                    <Posts/>
                 </Route>
             </Switch>
             </>

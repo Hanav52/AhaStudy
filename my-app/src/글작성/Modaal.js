622a4488136a3155c3b4c4871e8b1e9b763bd3b6
hanav52 <wnsrl8329@gmail.com>
2022-07-19 14:39:08 +0900

    글작성 클릭 시 로그인 확인

--- my-app/src/글작성/Modaal.js
+++ my-app/src/글작성/Modaal.js
@@ -12,6 +12,9 @@ import Typography from '@mui/material/Typography';
 import App from './Modal';
 import axios from "axios";
 import { useState } from 'react';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import moment from 'moment';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
@@ -72,7 +75,36 @@ export default function CustomizedDialogs() {
   };
 
   const handleClickOpen = () => {
-    setOpen(true);
+    if(visible === null) {
+      alert("로그인 후 이용해주세요.");
+    } else {
+      alert("글 작성하기");
+      // 400000이하로 떨어지면
+      if(diff2 < 400000) {
+        axios.post("http://bestinwoo.hopto.org:8080/auth/reissue", {
+          accessToken : AccessToken,
+          refreshToken: RefreshToken
+      })
+      .then(function (response) {
+        localStorage.removeItem("AccessToken");
+        localStorage.removeItem("AccessTokenExpiresIn");
+        localStorage.removeItem("RefreshToken");
+        localStorage.removeItem("RefreshTokenExpiresIn");
+        localStorage.setItem("AccessToken", response.data.accessToken);
+        localStorage.setItem("AccessTokenExpiresIn", response.data.accessTokenExpiresIn);
+        localStorage.setItem("RefreshToken", response.data.refreshToken);
+        localStorage.setItem("RefreshTokenExpiresIn", response.data.refreshTokenExpiresIn);
+        // 글작성하는 곳으로
+        setOpen(true);
+      }).catch(function (error) {
+        console.log(error)
+      }).then(function() {
+          // 항상 실행
+      });
+      } else {
+        // 글 작성하는 곳으로 이동
+      }
+    }
   };
   const handleClose = () => { 
     setOpen(false);
@@ -100,13 +132,33 @@ export default function CustomizedDialogs() {
     axios.post("http://bestinwoo.hopto.org:8080/post", formData, config).then(function (response) {
       console.log(response)
     }).catch(function (error) {
-        // 오류발생시 실행
     }).then(function() {
         // 항상 실행
     }); 
 
   };
-
+  // 토큰 재발행
+  const history = useHistory();
+  const t2 = new Date();
+// localstorage에서 데이터 받아오기
+  const [visible, setVisible] = useState();
+  const [LoginId, setLoginId] = useState();
+  const [AccessToken, setAccessToken] = useState();
+  const [AccessTokenExpiresIn, setAccessTokenExpiresIn] = useState();
+  const [RefreshToken, setRefreshToken] = useState();
+  const [RefreshTokenExpiresIn, setRefreshTokenExpiresIn] = useState();
+  // 날짜가 만료되면 액세스 토큰과 리프레쉬 토큰 api로 재발급받아 저장한다.
+  useEffect(() => {
+    setVisible(window.localStorage.getItem("State"));
+    setLoginId(window.localStorage.getItem("LoginId"));
+    setAccessToken(window.localStorage.getItem("AccessToken"));
+    setAccessTokenExpiresIn(window.localStorage.getItem("AccessTokenExpiresIn"));
+    setRefreshToken(window.localStorage.getItem("RefreshToken")); 
+    setRefreshTokenExpiresIn(window.localStorage.getItem("RefreshTokenExpiresIn"));
+},[]);
+  const t1 = Number(AccessTokenExpiresIn);
+  const diff2 = moment.duration(t1 - t2).asMilliseconds(); // 400000이하로 떨어지면
+  
   return (
     <div>
       <Button variant="outlined" onClick={handleClickOpen}>

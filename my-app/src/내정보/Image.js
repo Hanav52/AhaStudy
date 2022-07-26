adba85b88ebdfc1560842a4d5a9fd70bc7412db5
hanav52 <wnsrl8329@gmail.com>
2022-07-27 02:46:25 +0900

    프로필 이미지 가져오기 및 변경

---
+++ my-app/src/내정보/Image.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import normal from './normal.png'
+
+const Image = () => {
+  // useRef를 이용해 input태그에 접근한다.
+  const imageInput = useRef();
+  console.log(imageInput)
+  const [image, setImage] = useState("");
+
+  //api instance 생성
+  const instance = axios.create({
+    baseURL: 'http://bestinwoo.hopto.org:8080/',
+  });
+  // api header 부분 토큰이 들어가있다.
+    const config = {
+    headers: {
+    'Authorization': 'Bearer ' + localStorage.getItem("AccessToken"),
+    'Content-Type': 'multipart/form-data'
+    }};
+  // image api 부분
+
+  const onImgInputBtnClick = (e) => {
+    e.preventDefault();
+    imageInput.current.click();
+  }
+
+  const onImgChange = async (e) => {
+    const fm = new FormData();
+    console.log(e.target.files[0])
+    fm.append("file", e.target.files[0]);
+    instance.patch(`/user/img/${localStorage.getItem("UserId")}`, fm, config)
+    .then(function (response) {
+      localStorage.setItem("imageId",response.data.data)
+    }).catch(function (error) {
+      console.log(error)
+    }).then(function() {
+        // 항상 실행
+    }); 
+  }
+
+  useEffect(()=> {
+    instance.get(`/image/${localStorage.getItem("imageId")}`)
+      .then(function (response) {
+        console.log(response.request.responseURL)
+        setImage(response.request.responseURL)
+        window.localStorage.setItem("URL", response.request.responseURL)
+    })
+  },[])
+
+  // 프로필 가져오기
+  const [detail, setDetail] = useState([]);
+
+  useEffect(()=> {
+      try {
+      instance.get(`/user/${localStorage.getItem("UserId")}`)
+      .then(function(response) {
+        console.log(response.data.data);
+        setDetail(response.data.data)
+      })} catch(ex){
+        console.log("오류")
+      }
+    },[])
+
+  // input태그는 display:"none" 을 이용해 안보이게 숨겨준다.
+  return (
+  	<>
+      <input ref={imageInput} type="file" className="imgInput" id="logoImg" accept="image/*" name="file" onChange={onImgChange}multiple="multiple" style={{ display: "none" }}/>
+      <button onClick={onImgInputBtnClick}>이미지업로드</button>
+      <img className="lftimg" src={detail.profileImagePath === null ? normal : image} alt="프로필이미지"></img>
+	</>
+  );
+};
+
+export default Image;
--- my-app/src/내정보/MyProfile.js
+++ my-app/src/내정보/MyProfile.js
@@ -2,13 +2,12 @@ import { Box, Tabs } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BrowserRouter, Route, useParams } from "react-router-dom";
+import Image from "./Image";
+import App from "./Image";
 import BasicTabs from "./BasicTabs";
-import normal from './normal.PNG'
 import UpdateIntro from "./UpdateIntro";
 
 function MyProfile() {
-    // image 기본
-    const [image, setImage] = useState(normal);
 
     // 프로필 가져오기
     const [detail, setDetail] = useState([]);
@@ -16,11 +15,6 @@ function MyProfile() {
     const instance = axios.create({
         baseURL: 'http://bestinwoo.hopto.org:8080/',
       });
-    // api header 부분 토큰이 들어가있다.
-        const config = {
-        headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem("AccessToken"),
-        }};
     useEffect(()=> {
         try {
         instance.get(`/user/${localStorage.getItem("UserId")}`)
@@ -31,37 +25,6 @@ function MyProfile() {
           console.log("오류")
         }
       },[])
-    // image api 부분
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        e.persist();
-    
-        let files = e.target.profile_files.files;
-        let formData = new FormData();
-        for (let i = 0; i < files.length; i++) {
-          formData.append("file", files[i]);
-        }
-        const token = window.localStorage.getItem("AccessToken");
-        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-        const Title = window.localStorage.getItem("titlevalue");
-        const TagStroage = window.localStorage.getItem("localTags");
-        const ContentValue = window.localStorage.getItem("contentvalue");
-        formData.append("title", Title);
-        formData.append("content", ContentValue);
-        formData.append("boardId", window.localStorage.getItem("category"));
-        formData.append("tags", TagStroage);
-      
-    
-        axios.post("http://bestinwoo.hopto.org:8080/post", formData, config)
-        .then(function (response) {
-          console.log(response)
-        }).catch(function (error) {
-          console.log(error)
-        }).then(function() {
-            // 항상 실행
-        }); 
-    
-      };
   
   // html 부분
   return (
@@ -72,17 +35,7 @@ function MyProfile() {
                 <div className="profile-content">
                     <div className="MyProfileBody1">
                         <div className="leftImage">
-                        <form onSubmit={(e) => onSubmit(e)}>
-                            <input
-                            type="file"
-                            style={{display:'none'}}
-                            name="profile_files"
-                            multiple="multiple"
-                            accept='image/jpg,impge/png,image/jpeg'
-                            />
-                            <button type="submit">업로드</button>
-                        </form>
-                            <img className="lftimg" src={detail.profileImagePath === null ? image : detail.profileImagePath} alt="기본이미지"></img>
+                            <Image/>
                         </div>
                         <div className="rightContent">
                             <div className="rightHeader">

0b00e14aea1122eb6a1e7a82584219b2b7ae4dda
hanav52 <wnsrl8329@gmail.com>
2022-07-29 01:22:22 +0900

    stomp 설치

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import Notification from './알림/Notification';
 import Hole from './전체/통합 copy';
 ReactDOM.render(
   <React.StrictMode>
-    <Hole/>
+    <Notification/>
   </React.StrictMode>,
   document.getElementById('root')
 );

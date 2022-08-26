1c150941645fb9b5c692a4166b889fcc9a0a23c9
hanav52 <wnsrl8329@gmail.com>
2022-08-26 14:50:35 +0900

    로그인 후 커서 놓을 시 글씨 변경

--- my-app/src/NavBody/로그인후.js
+++ my-app/src/NavBody/로그인후.js
@@ -104,21 +104,9 @@ export default function AccountMenu() {
   return (
     <div>
       <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
-        {/* <Tooltip title="Notifications">
-            <IconButton
-              onClick={handleClick1}
-              size="small"
-              sx={{ ml: 2 }}
-              aria-controls={open1 ? "account-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open1 ? "true" : undefined}
-            >
-              <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
-          </IconButton>
-        </Tooltip> */}
         <Notification/>
         <Typography sx={{ minWidth: 100 }}>{LoginId}</Typography>
-        <Tooltip title="Account settings">
+        <Tooltip title="계정 설정">
           <IconButton
             onClick={handleClick}
             size="small"
@@ -131,49 +119,6 @@ export default function AccountMenu() {
           </IconButton>
         </Tooltip>
       </Box>
-      {/* <Menu
-        anchorEl={anchorEl1}
-        id="account-menu"
-        open={open1}
-        onClose={handleClose1}
-        onClick={handleClose1}
-        PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-            mt: 1.5,
-            "& .MuiAvatar-root": {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1
-            },
-            "&:before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0
-            }
-          }
-        }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-      >
-        
-        <MenuItem>
-          <ListItemIcon>
-            <PersonAdd fontSize="small" />
-          </ListItemIcon>
-          Add another account
-        </MenuItem>
-      </Menu> */}
       <Menu
         anchorEl={anchorEl}
         id="account-menu"
